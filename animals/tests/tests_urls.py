7dcabffb1cbdb0095b030f037423ac7934a56dce
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2023-01-04 08:43:11 -0300

    feat: Class 04.
    Until 04.01

---
+++ animals/tests/tests_urls.py
@@ -0,0 +1,17 @@
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from animals.views import index
+
+
+class AnimalsURLSTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_route_view_index(self):
+        request = self.factory.get('/')
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
+
+    # def test_route_view_with_reverse(self):
+    #     root = reverse('/')
+    #     self.assertEqual(root, index)
--- setup/tests.py
+++ setup/tests.py
@@ -11,18 +11,6 @@ class AnimalsTestCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_open_alura(self):
-        self.browser.get('https://alura.com.br')
-        # sleep(5)
-
-    # def test_open_live_server(self):
-    #     self.browser.get(self.live_server_url)
-    #     sleep(5)
-
-    def test_ooopppsss(self):
-        """Error example"""
-        self.fail('OOOPPPSSS... no good!!')
-
     def test_search_animal(self):
         """Testing if a user can find an animal in search screen"""
         home_page = self.browser.get(self.live_server_url + '/') # FIXME doesn't return anything
